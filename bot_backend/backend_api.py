d6ecae65638954e3908162a04e584a5449c2f242
Kazer0g <kazerog.workspace@gmail.com>
2025-06-14 01:59:35 +0300

    + Simple API

---
+++ bot_backend/backend_api.py
@@ -0,0 +1,14 @@
+import os
+from fastapi import FastAPI
+import logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+
+app = FastAPI()
+
+@app.get("/")
+def home_page():
+    return {"message": "Home Page"}
+
+@app.post("/users/register")
+def register_user():
+    logging.warning("User registration is not completed")
