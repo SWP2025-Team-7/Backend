e23c4e8aea171a865a4d6fb8a79c86405c386715
Kazer0g <kazerog.workspace@gmail.com>
2025-06-14 16:37:53 +0300

    Move commit

--- bot_backend/backend_api.py
+++ bot_backend/backend_api.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
@@ -9,7 +9,16 @@ app = FastAPI()
 def home_page():
     return {"message": "Home Page"}
 
-@app.post("/users/register")
+@app.post("/users/register", status_code=status.HTTP_200_OK)
 def register_user():
     logging.warning("User registration is not completed")
+    return 
 
+
+@app.post("/documents/upload", status_code=status.HTTP_200_OK)
+async def upload_document():
+    logging.warning("Uploading documents is not completed")
+
+@app.get("/documents/extract", status_code=status.HTTP_200_OK)
+async def extract_document():
+    logging.warning("Extraction documents is not completed")
