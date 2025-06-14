ae970f50fb2f369ecc4be36dd283b027f600e818
sooboal <s1oo1n22@yandex.ru>
2025-06-14 20:41:35 +0300

    +PDF sending

--- bot_backend/backend_api.py
+++ bot_backend/backend_api.py
@@ -1,24 +1,36 @@
 import os
-from fastapi import FastAPI, status, HTTPException
+import base64
+from fastapi import FastAPI, status, HTTPException, Request
+from pydantic import BaseModel
 import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 app = FastAPI()
 
+class UserRegistration(BaseModel):
+    userId: int
+    alias: str
+    
+class DocumentUploading(BaseModel):
+    userId: int
+    file_in_bytes: str
+
 @app.get("/")
 def home_page():
     return {"message": "Home Page"}
 
-@app.post("/users/register", status_code=status.HTTP_200_OK)
-def register_user():
+@app.get("/users/register", status_code=status.HTTP_200_OK)
+def register_user(r: UserRegistration):
+    logging.info(f"Data for registrating: userId:{r.userId}, alias:{r.alias}")
     logging.warning("User registration is not completed")
-    return 
+    return {"message": ""}
 
 
-@app.post("/documents/upload", status_code=status.HTTP_200_OK)
-async def upload_document():
+@app.get("/documents/upload", status_code=status.HTTP_200_OK)
+async def upload_document(r: DocumentUploading):
+    logging.info(f"Upploading file from userId: {r.userId}")
+    with open("file.pdf", "wb") as f:
+        f.write(r.file_in_bytes.encode("latin-1"))
     logging.warning("Uploading documents is not completed")
+    
 
-@app.get("/documents/extract", status_code=status.HTTP_200_OK)
-async def extract_document():
-    logging.warning("Extraction documents is not completed")
